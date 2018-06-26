be1da779d1ea505a0b030144f1af69180345469a
Sean Altemose <sean@seanaltemose.com>
2018-06-26 19:05:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
     if @@items.include?(item)
         resp.write item.price
         resp.status 200
-    else 
+    else
       resp.write "Item not found"
       resp.status 400
     end
