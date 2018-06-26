7b6f7102a7202f317e231de79375d4ec9df45a9c
Sean Altemose <sean@seanaltemose.com>
2018-06-26 19:08:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items/"
+    if req.path.match(/items/)
 
     item = req.path.split(/items/).last
     if @@items.include?(item)
