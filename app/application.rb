3b5339b825bb043c25f73d665601ad3a4d30f496
Sean Altemose <sean@seanaltemose.com>
2018-06-26 19:03:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
 
   @@items = []
-  
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -9,6 +9,14 @@ class Application
     if req.path.match(/items/)
 
     item = req.path.split(/items/).last
-    if
+    if @@items.include?(item)
+        resp.write item.price
+    else resp.status 400
+    end
+    else resp.status 404
+    end
+    resp.finish
+  end
+
 
 end
