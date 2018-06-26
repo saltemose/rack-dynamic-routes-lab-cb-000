0ae6cbce12a7c9bbb31aedab2f2423066f76e161
Sean Altemose <sean@seanaltemose.com>
2018-06-26 19:21:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if @@items.find {|i| i.name == item_name}
+      if @@items.include?(item_name)
         item = @@items.find {|i| i.name == item_name}
         resp.write item.price
     else
