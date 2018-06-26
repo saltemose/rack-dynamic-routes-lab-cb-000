0260e5b52eb09c07afd17245cffbc17c37e1a804
Sean Altemose <sean@seanaltemose.com>
2018-06-26 19:18:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if Item.include?(item_name)
-        item = Item.all.find {|i| i.name = item_name}
+        item = @@items.all.find {|i| i.name = item_name}
         resp.write item.price
     else
       resp.write "Item not found"
